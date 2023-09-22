3cf6943b36a7ff257827e4934754b00cc24be150
salleeem <mateusonego@gamil.com>
2023-09-22 13:48:10 -0300

    resto

--- JogoDaVelha/Script.js
+++ JogoDaVelha/Script.js
@@ -1,8 +1,11 @@
+// Selecionando o elemento HTML com a classe "QuemJoga"
 const QuemJoga = document.querySelector(".QuemJoga");
 
-let Escolha;
-let Jogador = "X";
+// Declarando variáveis globais
+let Escolha; // Armazena os movimentos dos jogadores
+let Jogador = "X"; // Controla de quem é a vez
 
+// Definindo as posições de vitória no jogo da velha
 let positions = [
   [1, 2, 3],
   [4, 5, 6],
@@ -14,52 +17,61 @@ let positions = [
   [3, 5, 7],
 ];
 
+// Função de inicialização do jogo
 function init() {
-  Escolha = [];
+  Escolha = []; // Inicializa o array de movimentos
 
+  // Define a mensagem indicando de quem é a vez
   QuemJoga.innerHTML = `Jogada de: ${Jogador}`;
 
+  // Limpa o conteúdo dos botões do jogo e adiciona eventos de clique
   document.querySelectorAll(".main button").forEach((item) => {
     item.innerHTML = "";
-    item.addEventListener("click", Movimento);
+    item.addEventListener("click", Movimento); // Quando clicados, chama a função Movimento
   });
 }
 
+// Inicializa o jogo quando a página carrega
 init();
 
+// Função que lida com os movimentos dos jogadores
 function Movimento(e) {
-  const index = e.target.getAttribute("data-i");
-  e.target.innerHTML = Jogador;
-  e.target.removeEventListener("click", Movimento);
-  Escolha[index] = Jogador;
+  const index = e.target.getAttribute("data-i"); // Pega o índice do botão clicado
+  e.target.innerHTML = Jogador; // Exibe o símbolo do jogador atual no botão
+  e.target.removeEventListener("click", Movimento); // Remove o evento de clique para impedir novas jogadas no botão clicado
+  Escolha[index] = Jogador; // Armazena a jogada no array de movimentos
 
   setTimeout(() => {
-    check();
+    check(); // Após um breve intervalo, verifica se alguém venceu ou houve empate
   }, [100]);
 
-  Jogador = Jogador === "X" ? "O" : "X";
-  QuemJoga.innerHTML = `Jogada de: ${Jogador}`;
+  Jogador = Jogador === "X" ? "O" : "X"; // Alterna o jogador atual
+  QuemJoga.innerHTML = `Jogada de: ${Jogador}`; // Atualiza a mensagem de vez do jogador
 }
 
+// Função que verifica se alguém ganhou ou houve empate
 function check() {
-  let UltimaJogada= Jogador === "X" ? "O" : "X";
+  let UltimaJogada = Jogador === "X" ? "O" : "X"; // Obtém o símbolo do jogador anterior
 
+  // Filtra os movimentos do jogador anterior
   const items = Escolha
     .map((item, i) => [item, i])
     .filter((item) => item[0] === UltimaJogada)
     .map((item) => item[1]);
 
+  // Verifica se o jogador anterior ganhou com base nas posições de vitória
   for (pos of positions) {
     if (pos.every((item) => items.includes(item))) {
-      alert("O jogador '" + UltimaJogada + "' ganhou a partida!");
-      init();
+      alert("O jogador '" + UltimaJogada + "' ganhou a partida!"); // Exibe um alerta com o vencedor
+      init(); // Reinicia o jogo
       return;
     }
   }
 
+  // Verifica se houve empate (todos os espaços estão preenchidos)
   if (Escolha.filter((item) => item).length === 9) {
-    alert("Empate!");
-    init();
+    alert("Empate!"); // Exibe um alerta de empate
+    init(); // Reinicia o jogo
     return;
   }
 }
--- Semáforo/Script.js
+++ Semáforo/Script.js
@@ -1,13 +1,22 @@
+// Seleciona todos os elementos com a classe 'luces-circulo' e armazena-os em uma NodeList
 const $lucesDelCirculo = document.querySelectorAll('.luces-circulo');
+
+// Inicializa um contador para controlar qual luz está atualmente visível
 let contadorDeLuz = 0;
 
-const mostrarLuz = () =>{
+// Esta função será chamada a cada 2 segundos para alternar as luzes
+const mostrarLuz = () => {
+    // Remove a classe atual para ocultar a luz atual
     $lucesDelCirculo[contadorDeLuz].className = 'luces-circulo';
+
+    // Incrementa o contador e reinicia para 0 se ele passar de 2
     contadorDeLuz++;
+    if (contadorDeLuz > 2) contadorDeLuz = 0;
 
-    if(contadorDeLuz > 2) contadorDeLuz = 0;
-
+    // Obtém a luz atual com base no contador e adiciona uma classe correspondente à sua cor
     const luzActual = $lucesDelCirculo[contadorDeLuz];
-    luzActual.classList.add(luzActual.getAttribute('color'))
+    luzActual.classList.add(luzActual.getAttribute('color'));
 }
-setInterval(mostrarLuz,2000)
+
+// Chama a função 'mostrarLuz' a cada 2000 milissegundos (2 segundos)
+setInterval(mostrarLuz, 2000);
